382d8072176f28fe0456cb2c8f88f249a56c127e
ka1tzyu <ka1tzyu@gmail.com>
2022-05-05 15:04:11 +0300

    feat(main): add settings tabs and default settings

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
-import {App, Notice, Plugin, PluginSettingTab} from 'obsidian';
-import findTaggedFiles from "./utils";
+import {App, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+interface BlockMetadata {
+	id: number;
+	author: string;
+	lastUpdate: number;
+	reloadInterval: number;
+	customClass: string;
+}
 
 interface LocalQuotesSettings {
 	quoteTag: string;
-	lastReload: number;
-	reloadInterval: number;
-	quotes: Map<string,string[]>;
+	defaultReloadInterval: number;
+	showReloadButton: boolean;
+	quoteVault: Map<string,string[]>;
+	blockMetadata: BlockMetadata[];
 }
 
 const DEFAULT_SETTINGS: LocalQuotesSettings = {
 	quoteTag: 'quotes',
-	lastReload: Date.now(),
-	reloadInterval: 86400,
-	quotes: null,
+	defaultReloadInterval: 86400,
+	showReloadButton: false,
+	quoteVault: new Map<string, string[]>(),
+	blockMetadata: new Array<BlockMetadata>(),
 }
 
 export default class LocalQuotes extends Plugin {
@@ -22,9 +29,12 @@ export default class LocalQuotes extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		await this.addSettingTab(new LocalQuotesSettingTab(this.app, this));
 	}
 
-	onunload() {}
+	async onunload() {
+		await this.saveSettings();
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -48,5 +58,35 @@ class LocalQuotesSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Quote tag')
+			.setDesc('Tag name that will be used for searching notes with quotes')
+			.addText(text => text
+				.setValue(this.plugin.settings.quoteTag)
+				.onChange(async (value) => {
+					this.plugin.settings.quoteTag = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Reload interval')
+			.setDesc('You can set default reload interval and miss corresponding field in codeblock')
+			.addText(text => text
+				.setValue(this.plugin.settings.defaultReloadInterval.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.defaultReloadInterval = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Show reload button')
+			.setDesc('Turn off if you want to hide reload button')
+			.addToggle(state => state
+				.setValue(this.plugin.settings.showReloadButton)
+				.onChange(async (value) => {
+					this.plugin.settings.showReloadButton = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
