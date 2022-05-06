083ea987b05e33b2e31f26e4f4326b35d5f77f3b
ka1tzyu <ka1tzyu@gmail.com>
2022-05-06 16:23:18 +0300

    fix(consts): allowing special characters in author and quote strings

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
-export const author_regexp = /:::\w+:::/gm;
-export const quote_regexp = /(- \w+)|(\d. \w+)/gm;
+export const author_regexp = /:::.+:::/gm;
+export const quote_regexp = /(- .+)|(\d. .+)/gm;
 
 export const codeblock_id_regexp = /id \w+/gm;
-export const codeblock_author_regexp = /author \w+/gm;
+export const codeblock_author_regexp = /author .+/gm;
 export const codeblock_reloadInterval_regexp = /reload \d+\w/gm;
 export const codeblock_customClass_regexp = /class \w+/gm;
 
