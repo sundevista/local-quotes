a2e9995c4156fcedf4e0b37b4f641e2e59eaef58
ka1tzyu <ka1tzyu@gmail.com>
2022-05-07 14:10:51 +0300

    test: add parseTime() tests

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -1,5 +1,17 @@
 import {parseTime} from "../util/parser";
 
-test('parse 8m conversation equal 480', () => {
+
+/*
+** PARSER TESTING
+*/
+test('checking parserTime()', () => {
+	expect(parseTime('8j')).toBe(8);
+	expect(parseTime('8')).toBe(8);
+	expect(parseTime('8s')).toBe(8);
 	expect(parseTime('8m')).toBe(480);
+	expect(parseTime('8h')).toBe(28800);
+	expect(parseTime('8d')).toBe(691200);
+	expect(parseTime('8w')).toBe(4838400);
+	expect(parseTime('8M')).toBe(21037968);
+	expect(parseTime('8y')).toBe(252455616);
 });
